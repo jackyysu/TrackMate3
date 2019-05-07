f7d9c10f398e533c96477b50a174455e13d33e6f
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-05-07 17:44:14 +0200

    De/serialize the Mamut feature computer state.
    The feature update state (GraphUpdateStack) is deserialized from
    a file along with the other features, then is fed to the
    MamutFeatureComputerService for future use.

--- src/main/java/org/mastodon/revised/model/mamut/MamutRawFeatureModelIO.java
+++ src/main/java/org/mastodon/revised/model/mamut/MamutRawFeatureModelIO.java
@@ -2,6 +2,7 @@ package org.mastodon.revised.model.mamut;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -15,10 +16,13 @@ import org.mastodon.feature.FeatureModel;
 import org.mastodon.feature.FeatureSpec;
 import org.mastodon.feature.io.FeatureSerializationService;
 import org.mastodon.feature.io.FeatureSerializer;
+import org.mastodon.feature.update.GraphUpdateStack;
+import org.mastodon.feature.update.GraphUpdateStackSerialization;
 import org.mastodon.graph.io.RawGraphIO.FileIdToGraphMap;
 import org.mastodon.graph.io.RawGraphIO.GraphToFileIdMap;
 import org.mastodon.io.FileIdToObjectMap;
 import org.mastodon.io.ObjectToFileIdMap;
+import org.mastodon.mamut.feature.MamutFeatureComputerService;
 import org.mastodon.project.MamutProject.ProjectReader;
 import org.mastodon.project.MamutProject.ProjectWriter;
 import org.scijava.Context;
@@ -33,6 +37,15 @@ public class MamutRawFeatureModelIO
 			final ProjectWriter writer )
 			throws IOException
 	{
+		final MamutFeatureComputerService computerService = context.getService( MamutFeatureComputerService.class );
+		final GraphUpdateStack< Spot, Link > graphUpdateStack = computerService.getGraphUpdateStack();
+		try (
+				final OutputStream fos = writer.getFeatureOutputStream( "GraphUpdateStack" );
+				final ObjectOutputStream oos = new ObjectOutputStream( new BufferedOutputStream( fos, 1024 * 1024 ) ))
+		{
+			GraphUpdateStackSerialization.serialize( graphUpdateStack, idmap, oos );
+		}
+
 		final FeatureSerializationService featureSerializationService = context.getService( FeatureSerializationService.class );
 		for ( final FeatureSpec< ?, ? > spec : featureModel.getFeatureSpecs() )
 		{
@@ -53,23 +66,33 @@ public class MamutRawFeatureModelIO
 
 	public static void deserialize(
 			final Context context,
-			final FeatureModel featureModel,
+			final Model model,
 			final FileIdToGraphMap< Spot, Link > idmap,
-			final RefCollection< Spot > vertices,
-			final RefCollection< Link > edges,
-			final String spaceUnits,
-			final String timeUnits,
 			final ProjectReader reader ) throws ClassNotFoundException, IOException
 	{
+		final MamutFeatureComputerService computerService = context.getService( MamutFeatureComputerService.class );
+		try (
+				final InputStream fis = reader.getFeatureInputStream( "GraphUpdateStack" );
+				final ObjectInputStream ois = new ObjectInputStream( new BufferedInputStream( fis, 1024 * 1024 ) ))
+		{
+			final GraphUpdateStack< Spot, Link > graphUpdateStack = GraphUpdateStackSerialization.deserialize( model.getGraph(), idmap, ois );
+			computerService.setGraphUpdateStack( graphUpdateStack );
+		}
+		catch ( final FileNotFoundException fnfe )
+		{
+			// Do nothing special.
+		}
+
 		final FeatureSerializationService featureSerializationService = context.getService( FeatureSerializationService.class );
 		final Collection< String > featureKeys = reader.getFeatureKeys();
+		final FeatureModel featureModel = model.getFeatureModel();
 		featureModel.pauseListeners();
 		featureModel.clear();
 		for ( final String featureKey : featureKeys )
 		{
 			@SuppressWarnings( "rawtypes" )
 			final FeatureSerializer serializer = featureSerializationService.serializerFor( featureKey );
-			if ( null == serializer)
+			if ( null == serializer )
 			{
 				System.err.println( "Do not know how to deserialize the feature with key: " + featureKey );
 				continue;
@@ -79,9 +102,21 @@ public class MamutRawFeatureModelIO
 			@SuppressWarnings( "rawtypes" )
 			Feature feature;
 			if ( targetClass == Spot.class )
-				feature = read( serializer, idmap.vertices(), vertices, spaceUnits, timeUnits, reader );
+				feature = read(
+						serializer,
+						idmap.vertices(),
+						model.getGraph().vertices(),
+						model.getSpaceUnits(),
+						model.getTimeUnits(),
+						reader );
 			else if ( targetClass == Link.class )
-				feature = read( serializer, idmap.edges(), edges, spaceUnits, timeUnits, reader );
+				feature = read(
+						serializer,
+						idmap.edges(),
+						model.getGraph().edges(),
+						model.getSpaceUnits(),
+						model.getTimeUnits(),
+						reader );
 			else
 			{
 				System.err.println( "Do not know how to deserialize a feature that targets " + targetClass );
