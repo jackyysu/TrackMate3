53e9c4b2c447db4d7ce7d9c8cf155d5955483e23
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-05-10 13:37:30 +0200

    Don't worry about de/serializing the update stacks.
    Since they are serialized as features, we don't have to worry about
    de/serialization either.

--- src/main/java/org/mastodon/revised/model/mamut/MamutRawFeatureModelIO.java
+++ src/main/java/org/mastodon/revised/model/mamut/MamutRawFeatureModelIO.java
@@ -2,7 +2,6 @@ package org.mastodon.revised.model.mamut;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.ObjectInputStream;
@@ -16,13 +15,10 @@ import org.mastodon.feature.FeatureModel;
 import org.mastodon.feature.FeatureSpec;
 import org.mastodon.feature.io.FeatureSerializationService;
 import org.mastodon.feature.io.FeatureSerializer;
-import org.mastodon.feature.update.GraphUpdateStack;
-import org.mastodon.feature.update.GraphUpdateStackSerialization;
 import org.mastodon.graph.io.RawGraphIO.FileIdToGraphMap;
 import org.mastodon.graph.io.RawGraphIO.GraphToFileIdMap;
 import org.mastodon.io.FileIdToObjectMap;
 import org.mastodon.io.ObjectToFileIdMap;
-import org.mastodon.mamut.feature.MamutFeatureComputerService;
 import org.mastodon.project.MamutProject.ProjectReader;
 import org.mastodon.project.MamutProject.ProjectWriter;
 import org.scijava.Context;
@@ -37,15 +33,6 @@ public class MamutRawFeatureModelIO
 			final ProjectWriter writer )
 			throws IOException
 	{
-		final MamutFeatureComputerService computerService = context.getService( MamutFeatureComputerService.class );
-		final GraphUpdateStack< Spot, Link > graphUpdateStack = computerService.getGraphUpdateStack();
-		try (
-				final OutputStream fos = writer.getFeatureOutputStream( "GraphUpdateStack" );
-				final ObjectOutputStream oos = new ObjectOutputStream( new BufferedOutputStream( fos, 1024 * 1024 ) ))
-		{
-			GraphUpdateStackSerialization.serialize( graphUpdateStack, idmap, oos );
-		}
-
 		final FeatureSerializationService featureSerializationService = context.getService( FeatureSerializationService.class );
 		for ( final FeatureSpec< ?, ? > spec : featureModel.getFeatureSpecs() )
 		{
@@ -70,19 +57,6 @@ public class MamutRawFeatureModelIO
 			final FileIdToGraphMap< Spot, Link > idmap,
 			final ProjectReader reader ) throws ClassNotFoundException, IOException
 	{
-		final MamutFeatureComputerService computerService = context.getService( MamutFeatureComputerService.class );
-		try (
-				final InputStream fis = reader.getFeatureInputStream( "GraphUpdateStack" );
-				final ObjectInputStream ois = new ObjectInputStream( new BufferedInputStream( fis, 1024 * 1024 ) ))
-		{
-			final GraphUpdateStack< Spot, Link > graphUpdateStack = GraphUpdateStackSerialization.deserialize( model.getGraph(), idmap, ois );
-			computerService.setGraphUpdateStack( graphUpdateStack );
-		}
-		catch ( final FileNotFoundException fnfe )
-		{
-			// Do nothing special.
-		}
-
 		final FeatureSerializationService featureSerializationService = context.getService( FeatureSerializationService.class );
 		final Collection< String > featureKeys = reader.getFeatureKeys();
 		final FeatureModel featureModel = model.getFeatureModel();
