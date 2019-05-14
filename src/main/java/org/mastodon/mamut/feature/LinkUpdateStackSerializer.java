c4c0c8ac6344bab46ed3819302e939ce68040ca4
Jean-Yves TINEVEZ <jean-yves.tinevez@pasteur.fr>
2019-05-14 16:58:50 +0200

    New incremental feature framework for Mamut feature computation.
    Now that the incremental update stacks are stored as features in the
    feature model, we don't need to make something special in the
    computer service to provide them to the features that want to
    depends on it.

---
+++ src/main/java/org/mastodon/mamut/feature/LinkUpdateStackSerializer.java
@@ -0,0 +1,29 @@
+package org.mastodon.mamut.feature;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+
+import org.mastodon.collection.RefCollection;
+import org.mastodon.feature.update.UpdateStackSerializer;
+import org.mastodon.io.FileIdToObjectMap;
+import org.mastodon.mamut.feature.LinkUpdateStack.Spec;
+import org.mastodon.revised.model.mamut.Link;
+import org.scijava.plugin.Plugin;
+
+@Plugin( type = LinkUpdateStackSerializer.class )
+public class LinkUpdateStackSerializer extends UpdateStackSerializer< LinkUpdateStack, Link >
+{
+
+	@Override
+	public Spec getFeatureSpec()
+	{
+		return LinkUpdateStack.SPEC;
+	}
+
+	@Override
+	public LinkUpdateStack deserialize( final FileIdToObjectMap< Link > idmap, final RefCollection< Link > pool, final ObjectInputStream ois ) throws IOException, ClassNotFoundException
+	{
+		final DeserializedStruct< Link > struct = deserializeStruct( idmap, pool, ois );
+		return new LinkUpdateStack( pool, struct.stateStack, struct.currentUpdate );
+	}
+}
